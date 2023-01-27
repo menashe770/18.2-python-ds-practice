e4690d13bb9c146fe7f18ea9de288c0d2d141f63
Menashe <mborukhov770@gmail.com>
2023-01-27 00:35:34 -0700

    08 add done

--- 08_list_manipulation/list_manipulation.py
+++ 08_list_manipulation/list_manipulation.py
@@ -46,3 +46,11 @@ def list_manipulation(lst, command, location, value=None):
             return lst.pop(0)
         elif location == 'end':
             return lst.pop()
+
+    elif command == 'add':
+        if location == 'beginning':
+            lst.insert(0, value)
+            return lst
+        elif location == 'end':
+            lst.append(value)
+            return lst
