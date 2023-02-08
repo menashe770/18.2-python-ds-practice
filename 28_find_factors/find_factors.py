bf204cbe38e0bfc54f601abc27139d30a6e22bf2
Menashe <mborukhov770@gmail.com>
2023-02-08 15:46:12 -0700

    28

--- 28_find_factors/find_factors.py
+++ 28_find_factors/find_factors.py
@@ -13,3 +13,12 @@ def find_factors(num):
     >>> find_factors(321421)
     [1, 293, 1097, 321421]
     """
+
+    # result = []
+    # for n in range(1, num + 1):
+    #     if num % n == 0:
+    #         result.append(n)
+
+    result = [n for n in range(1, num + 1) if num % n == 0]
+
+    return result
