4cfec7380e56af089b8e865f3fc0c0ee76e59327
Menashe <mborukhov770@gmail.com>
2023-02-07 17:02:58 -0700

    27

--- 27_titleize/titleize.py
+++ 27_titleize/titleize.py
@@ -7,3 +7,7 @@ def titleize(phrase):
         >>> titleize('oNLy cAPITALIZe fIRSt')
         'Only Capitalize First'
     """
+
+    return phrase.title()
+
+    # return ' '.join([s.capitalize() for s in phrase.split(' ')])
