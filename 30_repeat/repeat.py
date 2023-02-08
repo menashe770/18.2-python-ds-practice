7f7982e49f0e7150fe8386323f8409acf89a6b8f
Menashe <mborukhov770@gmail.com>
2023-02-08 16:33:36 -0700

    30

--- 30_repeat/repeat.py
+++ 30_repeat/repeat.py
@@ -18,3 +18,13 @@ def repeat(phrase, num):
         >>> repeat('abc', 'nope') is None
         True
     """
+
+    # if not isinstance(num, int) or num < 0:
+    #     return None
+
+    # return phrase * num
+
+    if isinstance(num, int) and num >= 0:
+        return phrase * num
+    else:
+        return None
